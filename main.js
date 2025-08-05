81cba0745315fd2dbf07b213b9e2fcf647fad4e3
DiegoFatVega <fatvegadiego@gmail.com>
2025-08-05 18:24:47 +0200

    Replaced static list items with dynamic email data from Boolean API. Used Axios to perform 10 GET requests and appended results as Bootstrap-styled <li> elements inside the #list container.

--- main.js
+++ main.js
@@ -3,15 +3,28 @@ console.log('Hi')
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
-const api_url = 'https://flynn.boolean.careers/exercises/api/random/mail';
-axios.get(api_url)
+const listEl = document.getElementById("list");
+let email_arr = [];
+
+
+for (let i = 0; i < 10; i++) {
+  const api_url = 'https://flynn.boolean.careers/exercises/api/random/mail';
+    axios.get(api_url)
     .then(response => {
-        //codice per far qualcosa con la risposta
-        console.log(response);
-        const result = response.data;
-        console.log(result);
+        const email = response.data.response;
+        email_arr.push(email);
+        //creo un nuovo elemento <li> con le classi di bootstrap
+
+        const li = document.createElement('li');
+        li.className = 'list-group-item'
+        li.textContent = email;
+
+        //inserisco l`elemento nella lista
+        listEl.appendChild(li);
+        console.log(email_arr)
     })
     .catch(error =>{
         //codice da eseguir in caso di errore
         console.log(error);
-    });
+    });  
+}
